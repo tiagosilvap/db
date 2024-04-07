e6a88551e9220616a6f729f325b36acc3ffafda7
Tiago Pereira <tiago.pereira@hotmart.com>
2024-04-07 18:23:11 -0300

    add volume

---
+++ initdb/01-init.sql
@@ -0,0 +1,8 @@
+-- Arquivo: initdb/01-init.sql
+CREATE TABLE IF NOT EXISTS produto (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    descricao TEXT,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=INNODB;
